fe9102e8b765db046767ccac251d338a1d34cf58
Krisna Setyawan <krisn4setyawan@gmail.com>
2023-04-06 13:53:38 +0700

    merge tugas ferika (divisi, calon karyawan, absen, pengaturan user)

--- app/Views/absensi/index.php
+++ app/Views/absensi/index.php
@@ -10,7 +10,7 @@
 
     <div class="row mt-4">
         <div class="col-md-4">
-            <a class="text-decoration-none" href="<?= base_url() ?>view-absensi">
+            <a class="text-decoration-none" href="view-absensi">
                 <div class="card mb-3 shadow" style="border: 1px solid #1762A5;">
                     <h5 class="card-header" style="background-color: #1762A5; color: #fff;">Absensi Karyawan</h5>
                     <div class="card-body text-secondary">
@@ -24,7 +24,7 @@
                 <div class="card mb-3 shadow" style="border: 1px solid #1762A5;">
                     <h5 class="card-header" style="background-color: #1762A5; color: #fff;">Laporan</h5>
                     <div class="card-body text-secondary">
-                    <i class="fa-3x fa-regular fa-note-sticky"></i>
+                        <i class="fa-3x fa-regular fa-note-sticky"></i>
                     </div>
                 </div>
             </a>
--- app/Views/user/index.php
+++ app/Views/user/index.php
@@ -10,7 +10,7 @@
 
     <div class="row mt-4">
         <div class="col-md-4">
-            <a class="text-decoration-none" href="<?= base_url() ?>user-group-view">
+            <a class="text-decoration-none" href="user-group-view">
                 <div class="card mb-3 shadow" style="border: 1px solid #1762A5;">
                     <h5 class="card-header" style="background-color: #1762A5; color: #fff;">User Group</h5>
                     <div class="card-body text-secondary">
@@ -20,21 +20,21 @@
             </a>
         </div>
         <div class="col-md-4">
-            <a class="text-decoration-none" href="<?= base_url() ?>user-permission-view">
+            <a class="text-decoration-none" href="user-permission-view">
                 <div class="card mb-3 shadow" style="border: 1px solid #1762A5;">
                     <h5 class="card-header" style="background-color: #1762A5; color: #fff;">User Permission</h5>
                     <div class="card-body text-secondary">
-                    <i class="fa-3x fa-regular fa-circle-check"></i>
+                        <i class="fa-3x fa-regular fa-circle-check"></i>
                     </div>
                 </div>
             </a>
         </div>
         <div class="col-md-4">
-            <a class="text-decoration-none" href="<?= base_url() ?>group-permission-view">
+            <a class="text-decoration-none" href="group-permission-view">
                 <div class="card mb-3 shadow" style="border: 1px solid #1762A5;">
                     <h5 class="card-header" style="background-color: #1762A5; color: #fff;">Group Permission</h5>
                     <div class="card-body text-secondary">
-                    <i class="fa-3x fa-solid fa-users-rectangle"></i>
+                        <i class="fa-3x fa-solid fa-users-rectangle"></i>
                     </div>
                 </div>
             </a>
